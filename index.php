605ccca95c5d9a1770d66fbf9201660fc4ff7b06
Andrea <a.shcherbakov@ritoll.it>
2013-11-14 14:34:07 +0100

    the editor is set up

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+	<script type="text/javascript" src="js/jquery-2.0.3.min.js"></script>
+	<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+	<script type="text/javascript" src="ckeditor/adapters/jquery.js"></script>
+	<script type="text/javascript" src="js/start.js"></script>
+
+	<title>Creatore di newsletter</title>
+</head>
+<body>
+	<h1>
+		Newsletter
+	</h1>
+	<div>
+		<textarea name="editor">Comincia a scivere la newsletter qua.</textarea>
+	</div>
+
+
+
+</body>
+
+</html>
