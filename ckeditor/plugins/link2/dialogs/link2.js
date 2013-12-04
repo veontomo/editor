9d8115f6dbf157aef69f7a8acf6c300c23b599c9
Andrea <a.shcherbakov@ritoll.it>
2013-12-04 16:18:45 +0100

    dummy commit

--- ckeditor/plugins/link2/dialogs/link2.js
+++ ckeditor/plugins/link2/dialogs/link2.js
@@ -62,7 +62,8 @@ CKEDITOR.dialog.add("linkSimplified", function(editor) {
         onShow: function() {
             var node = this.getParentEditor().getSelection();
             var sel = node.getNative();
-            var href = dropProtocol(node.getStartElement().getAttribute('href'));
+            var hrefComplete = node.getStartElement().getAttribute('href');
+            var href = hrefComplete ? dropProtocol(hrefComplete) : '';
 
             this.setValueOf('tab-general', 'text', sel);
             this.setValueOf('tab-general', 'href', href);
@@ -87,84 +88,6 @@ CKEDITOR.dialog.add("linkSimplified", function(editor) {
                 aTag.setHtml(aTagContent);                
                 editor.insertElement(aTag);
             }
-        }
-    };
-});
-
-
-CKEDITOR.dialog.add("unlinkSimplified", function(editor) {
-    return {
-        title: editor.lang.link.info,
-        minWidth: 400,
-        minHeight: 300,
-        height: '20em',
-        contents: [{
-            id: 'tab-general',
-            label: 'labelTab1',
-            title: 'title tab 1',
-            elements: [{
-                type: 'hbox',
-                widths: ['10%', '90%'],
-                children: [{
-                    type: 'html',
-                    html: 'http://',
-                    style: 'padding-right: 0px; margin: 0; float: left; padding-top: 0.5em;'
-                }, {
-                    type: 'text',
-                    id: 'href',
-                    style: 'padding-left: 0px; margin: 0; float: left; width: 100%;'
-                }]
-            }, {
-                type: 'hbox',
-                widths: ['10%', '90%'],
-                children: [{
-                    type: 'html',
-                    html: 'Testo',
-                    style: 'padding-right: 0px; margin: 0; float: left; padding-top: 0.5em;'
-                }, {
-                    type: 'text',
-                    id: 'text',
-                    style: 'padding-left: 0px; margin: 0; float: left; width: 100%;'
-                }]
-            }, {
-                type: 'html',
-                html: editor.lang.link.styles + '<br>'
-            }, {
-                type: 'checkbox',
-                id: 'underlined',
-                label: editor.lang.basicstyles.underline,
-                "default": true,
-            }]
-        }],
-
-        onShow: function() {
-            var sel = this.getParentEditor().getSelection().getNative();
-            this.setValueOf('tab-general', 'text', sel);
-        },
-
-        onOk: function() {
-            // user input
-            var linkRaw = this.getValueOf('tab-general', 'href');
-            var linkText = this.getValueOf('tab-general', 'text');
-            var underlined = this.getValueOf('tab-general', 'underlined');
-
-            var link = 'http://' + linkRaw.replace(/http:\/\//i, '');
-
-            var stylesLink = new LinkAttributes();
-            stylesLink["text-decoration"] = underlined ? 'underline' : 'none';
-            var aTag = editor.document.createElement('a');
-            aTag.setAttribute('href', link);
-            aTag.setAttribute('style', stylesLink.toString());
-            aTag.setAttribute('target', '_blank');
-            aTag.setHtml(linkText);
-
-/*            // what to insert into <a href=""> ... </a>
-            var sel = this.getParentEditor().getSelection().getNative();
-            var aTagContent = sel ? sel : link;
-            console.log('aTagContent: ' + aTagContent);
-            aTag.setHtml(aTagContent);
-*/
-            editor.insertElement(aTag);
         }
     };
 });
