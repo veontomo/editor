3536180bea14de93e3204b310fb74c78ce626ca2
unknown <a.shcherbakov@ritoll.it>
2013-12-06 16:56:46 +0100

    alert is added when one attempts to load non-html file

--- ckeditor/plugins/upload/dialogs/upload.js
+++ ckeditor/plugins/upload/dialogs/upload.js
@@ -3,28 +3,42 @@ CKEDITOR.dialog.add('uploadDialog', function(editor) {
         title: editor.lang.common.upload,
         minWidth: 250,
         minHeight: 100,
+
         contents: [{
             id: 'uploadTab',
+            label: 'First Tab',
+            title: 'First Tab Title',
             elements: [{
-                type: 'text',
-                id: 'filepath',
-                label: 'Percorso per il file',
-                validate: function(){
-                    var filePath = this.getValue();
-                    alert('Name cannot be empty.' );
-                    return false;
-                    
-                }
+                type: 'vbox',
+                children: [{
+                    type: 'text',
+                    id: 'filepath',
+                    label: 'Percorso per il file',
+                    validate: function() {
+                        var filePath = this.getValue();
+                        if (fileExt(filePath) !== 'html') {
+                            var text = 'il file deve avere l\'estensione html!';
+                            CKEDITOR.document.getById('message').setHtml(text);
+                            return false;
+                        }
+                    }
+                }, {
+                    type: 'html',
+                    html: '<div class="warning" id="message"></div>',
+                }]
             }]
         }],
         onOk: function() {
             var filePath = this.getValueOf('uploadTab', 'filepath');
             console.log(filePath);
-            $.get(filePath, function(data){
+            $.get(filePath, function(data) {
                 console.log(data);
-            }).fail(function(){
+            }).fail(function() {
                 console.log(filePath + ' is not found');
             })
+        },
+        onShow: function() {
+            CKEDITOR.document.getById('message').setHtml('');
         }
 
     };
