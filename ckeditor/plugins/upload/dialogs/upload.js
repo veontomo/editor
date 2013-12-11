74bfc37b1350aef11cbc1c5df7d5e75808d96881
unknown <a.shcherbakov@ritoll.it>
2013-12-11 14:23:15 +0100

    fictiuos button is added to the upload form

--- ckeditor/plugins/upload/dialogs/upload.js
+++ ckeditor/plugins/upload/dialogs/upload.js
@@ -10,25 +10,38 @@ CKEDITOR.dialog.add('uploadDialog', function(editor) {
                 type: 'vbox',
                 children: [{
                     type: 'file',
-                    label: 'Percorso1',
-                    id: 'fileUpload'
+                    label: 'Percorso',
+                    id: 'fileUpload',
+                    size: 10,
+                    action: '#'
                 }, {
                     type: 'fileButton',
-                    label: 'Percorso2',
+                    label: 'Caricare',
                     'for': ['tab1', 'fileUpload'],
+                    onClick: function() {
+                        // adds a fictious button to submit the form and clicks it. Does not work!
+                        var elem = this.getDialog().getContentElement('tab1', 'fileUpload').getElement();
+                        var submitBtn = new CKEDITOR.dom.element('input');
+                        submitBtn.setAttribute('type', 'submit');
+                        submitBtn.setAttribute('value', 'submit');
+                        submitBtn.setAttribute('id', 'pulsanteFittizio');
+                        submitBtn.setAttribute('hidden', 'hidden');
+                        elem.append(submitBtn);
+                        $('#pulsanteFittizio').click();
+                        console.log(elem);
+                        this.getDialog().hide();
+                        return false;
+                    },
                     filebrowser: {
-                        onSelect: function( fileUrl, data ) {
-                            alert( 'Successfully uploaded: ' + fileUrl );
+                        onSelect: function(fileUrl, data) {
+                            alert('Successfully uploaded: ' + fileUrl);
                         }
                     }
                 }]
             }]
         }],
-        onOk: function() {
-        },
-        onShow: function() {
-            
-        }
+        onOk: function() {},
+        onShow: function() {}
 
     };
 });
