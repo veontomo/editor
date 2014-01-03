ece9d84c3bcd78ed19a5d7eea8d585560561dc7c
unknown <a.shcherbakov@ritoll.it>
2014-01-03 12:24:32 +0100

    yui api is generated for Attributes.js

---
+++ js/out/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Attributes",
+        "Cell",
+        "Content",
+        "LinkStyle",
+        "Row",
+        "Style",
+        "Table",
+        "TableAttributes",
+        "TextStyle"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
