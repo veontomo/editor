939584713effb820df107d50b5dce76581d204a9
unknown <a.shcherbakov@ritoll.it>
2014-02-25 09:21:16 +0100

    Object Mapping is added

---
+++ js/classes/mapping.js
@@ -0,0 +1,9 @@
+var Mapping = {
+	'table': {name: 'Table'},
+	'td': {name: 'Cell'},
+	'tr': {name: 'Row'},
+	'a': {name: 'Link'},
+	'li': {name: 'ListItem'},
+	'ul': {name: 'List', params: ['ul']},
+	'ol': {name: 'List', params: ['ol']}
+}
