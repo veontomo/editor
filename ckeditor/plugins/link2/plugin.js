0529516e0301d735e80e9b170d5f43f6a6e3324e
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-03-13 12:03:48 +0100

    non-DRY file is removed from link2/dialogs

--- ckeditor/plugins/link2/plugin.js
+++ ckeditor/plugins/link2/plugin.js
@@ -9,7 +9,6 @@ CKEDITOR.plugins.add('link2', {
 
 		// Define an editor command that opens our dialog.
 		editor.addCommand('link2', new CKEDITOR.dialogCommand('linkSimplified'));
-		editor.addCommand('modifyLink', new CKEDITOR.dialogCommand('modifyLinkSimplified'));
 		// Create a toolbar button that executes the above command.
 		editor.ui.addButton('link2', {
 			// The text part of the button (if available) and tooptip.
@@ -22,7 +21,6 @@ CKEDITOR.plugins.add('link2', {
 
 		// Register our dialog file. this.path is the plugin folder path.
 		CKEDITOR.dialog.add('linkSimplified', this.path + 'dialogs/link2.js');
-		// CKEDITOR.dialog.add('modifyLinkSimplified', this.path + 'dialogs/modifyLinkSimplified.js');
 
 		if (editor.contextMenu) {
 			editor.addMenuGroup('link2Group');
