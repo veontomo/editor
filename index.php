fbaf1f7d1a594b61004b0ec2b39990f8c5875bb2
Andrey Shcherbakov <a.shcherbakov@ritoll.it>
2014-03-19 11:58:19 +0100

    jQuery is taken from jquery CDN

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
 
-<!--	<script type="text/javascript" src="JSON-js/json2.js"></script> -->
-<!--	<script type="text/javascript" src="js/jquery-2.0.3.min.js"></script> -->
-	<script type="text/javascript" src="vendor/jquery-1.10.2.min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
 	<script type="text/javascript" src="ckeditor/adapters/jquery.js"></script>
 	<script type="text/javascript" src="js/start.js"></script>
