0e06e51a42663147942120b0db148b51a0081dd4
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-04-22 16:53:03 +0200

    Factory.factory is made protected in general_config.js

--- settings/general_config.js
+++ settings/general_config.js
@@ -5,8 +5,9 @@ var NEWSLETTER = {
 	'marker-name': 'data-marker', // an attribute to mark objects
 };
 
-var factory = new Factory(),
-	map = new Mapping();
+
+// rules to be used when creating instances by means of Factory() class
+var map = new Mapping();
 map.add(function(el){return el !== undefined && el.nodeType === Node.TEXT_NODE;}, PlainText);
 map.add(function(el){return el !== undefined && el.nodeType === Node.ELEMENT_NODE && el.tagName === 'TD';}, Cell);
 map.add(function(el){return el !== undefined && el.nodeType === Node.ELEMENT_NODE && el.tagName === 'TABLE';}, Table);
@@ -16,8 +17,17 @@ map.add(function(el){return el !== undefined && el.nodeType === Node.ELEMENT_NOD
 map.add(function(el){return el !== undefined && el.nodeType === Node.ELEMENT_NODE && el.tagName === 'OL';}, OList);
 map.add(function(el){return el !== undefined && el.nodeType === Node.ELEMENT_NODE && el.tagName === 'UL';}, UList);
 map.setDefaultTarget(Tag);
+
+var factory = new Factory();
 factory.setMapping(map);
 
 var FACTORY = {
-	factory: factory
+	// factory: factory
 };
+// it is possible to use Factory methods to modify FACTORY.factory
+// but it is prohibited to remove Factory.factory attribute
+Object.defineProperty(FACTORY, 'factory', {
+	value: factory,
+	writable: false
+});
+
