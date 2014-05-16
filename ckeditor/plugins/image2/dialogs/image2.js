9461444b7880c026e9714f6d5733ca5172d8ac8a
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-05-16 11:54:41 +0200

    Nothing gets inserted if the image returns empty html representation

--- ckeditor/plugins/image2/dialogs/image2.js
+++ ckeditor/plugins/image2/dialogs/image2.js
@@ -59,12 +59,16 @@ CKEDITOR.dialog.add( 'imageSimplified', function(editor) {
 			var textAlt = this.getValueOf('tab-general', 'textAlt'),
 				imageUrl = this.getValueOf('tab-general', 'imageUrl'),
 				img = new Image(),
-				imgObj;
+				imgObj, imgHtml;
 			img.setOrigin(imageUrl);
 			img.setAttrProperty('alt', textAlt);
 			img.setAttrProperty('title', textAlt);
-			imgObj = CKEDITOR.dom.element.createFromHtml(img.toHtml());
-			editor.insertElement(imgObj);
+			imgHtml = img.toHtml();
+			if (typeof imgHtml === 'string' && imgHtml.length > 0){
+				imgObj = CKEDITOR.dom.element.createFromHtml(imgHtml);
+				editor.insertElement(imgObj);
+			}
+
 		}
 	};
 });
