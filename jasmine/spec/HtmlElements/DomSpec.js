1ed369ffa5addb72b0260b0788913c0bd7bbc182
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-06-24 09:40:46 +0200

    Selection-irrelevant methods are moved to Dom class

---
+++ jasmine/spec/HtmlElements/DomSpec.js
@@ -0,0 +1,181 @@
+/*jslint plusplus: true, white: true */
+/*global describe, it, expect, spyOn, beforeEach, Dom */
+
+describe('Getting common parent', function(){
+        var n00, n10, n11, n20, n21, n22, n23, n30, n31, m00, m10, m11,
+            dom;
+//                    n00                                m00
+//         ____________|_________                    _____|____
+//         |                     |                   |         |
+//        n10                   n11                 m10       m11
+//   ______|______               |
+//   |     |      |              |
+//  n20   n21    n22            n23
+//      ___|____
+//      |       |
+//     n30     n31
+//
+        beforeEach(function(){
+            dom = new Dom();
+            n00 = document.createElement('div00');
+            n10 = document.createElement('div10');
+            n11 = document.createElement('div11');
+            n20 = document.createElement('div20');
+            n21 = document.createElement('div21');
+            n22 = document.createElement('div22');
+            n23 = document.createElement('div23');
+            n30 = document.createElement('div30');
+            n31 = document.createElement('div31');
+            m00 = document.createElement('div');
+            m10 = document.createElement('div');
+            m11 = document.createElement('div');
+            n00.appendChild(n10);
+            n00.appendChild(n11);
+            n10.appendChild(n20);
+            n10.appendChild(n21);
+            n10.appendChild(n22);
+            n21.appendChild(n30);
+            n21.appendChild(n31);
+            n11.appendChild(n23);
+            m00.appendChild(m10);
+            m00.appendChild(m11);
+        });
+
+        it('returns the first argument if it contains the second argument', function(){
+            expect(dom.commonAncestor(n10, n31)).toBe(n10);
+        });
+
+        it('returns the second argument if it contains the first argument', function(){
+            expect(dom.commonAncestor(n23, n00)).toBe(n00);
+        });
+
+
+        it('returns null if the nodes have no common parent', function(){
+            expect(dom.commonAncestor(n23, m10)).toBe(null);
+        });
+
+        it('returns the common parent if the nodes are siblings of each other', function(){
+            expect(dom.commonAncestor(n21, n22)).toBe(n10);
+        });
+
+        it('returns the common parent if the first argument is located deeper than the second', function(){
+            expect(dom.commonAncestor(n21, n22)).toBe(n10);
+        });
+
+        it('returns the common parent if the second argument is located deeper than the first', function(){
+            expect(dom.commonAncestor(n30, n23)).toBe(n00);
+        });
+    });
+
+    describe('Finds proxy node', function(){
+        var e00, e10, e11, t20, e21, t22, e23, t24, t25, e30, t31,
+            dom;
+//                    e00
+//         ____________|_________
+//         |                     |
+//        e10                   e11
+//   ______|______ ________      |
+//   |     |      |    |   |     |
+//  t20   e21    t22  e23 t24   t25
+//      ___|____
+//      |       |
+//     e30     t31
+
+        beforeEach(function(){
+            dom = new Dom();
+            e00 = document.createElement('div00');
+            e10 = document.createElement('div10');
+            e11 = document.createElement('div11');
+            t20 = document.createTextNode('text node 2.0');
+            e21 = document.createElement('div21');
+            t22 = document.createTextNode('text node 2.2');
+            e23 = document.createElement('div21');
+            t24 = document.createTextNode('text node 2.4');
+            t25 = document.createTextNode('text node 2.5');
+            e30 = document.createElement('div30');
+            t31 = document.createTextNode('text node 3.1');
+            e00.appendChild(e10);
+            e00.appendChild(e11);
+            e10.appendChild(t20);
+            e10.appendChild(e21);
+            e10.appendChild(t22);
+            e10.appendChild(e23);
+            e10.appendChild(t24);
+            e21.appendChild(e30);
+            e21.appendChild(t31);
+            e11.appendChild(t25);
+        });
+
+        it('gets proxy of an element node', function(){
+            expect(dom.proxy(e21)).toBe(e21);
+        });
+
+        it('gets proxy of a text node without siblings', function(){
+            expect(dom.proxy(t25)).toBe(e11);
+        });
+
+        it('gets proxy of a middle (among its siblings) text node', function(){
+            expect(dom.proxy(t22)).toBe(t22);
+        });
+
+        it('gets proxy of the first (among its siblings) text node', function(){
+            expect(dom.proxy(t20)).toBe(t20);
+        });
+        it('gets proxy of the last (among its siblings) text node', function(){
+            expect(dom.proxy(t24)).toBe(t24);
+        });
+    });
+
+
+    describe('Finds style in the ascendants', function(){
+        var e00, e10, e11, t20, e21, t22, e23, t24, t25, e30, t31,
+            dom;
+
+//                             e00 (block: wide)
+//         ____________________________|_________
+//         |                                     |
+//        e10                                   e11 (block: narrow)
+//   ______|_______________________________      |
+//   |     |                      |    |   |     |
+//  t20   e21 (block: narrow)    t22  e23 t24   t25
+//      ___|_________________
+//      |                    |
+//     e30 (block: wide)    t31
+
+        beforeEach(function(){
+            dom = new Dom();
+            var stl1 = 'class: media; block: wide;',
+                stl2 = 'size: biggest; block: narrow;';
+            e00 = document.createElement('div00');
+            e10 = document.createElement('div10');
+            e11 = document.createElement('div11');
+            t20 = document.createTextNode('text node 2.0');
+            e21 = document.createElement('div21');
+            t22 = document.createTextNode('text node 2.2');
+            e23 = document.createElement('div21');
+            t24 = document.createTextNode('text node 2.4');
+            t25 = document.createTextNode('text node 2.5');
+            e30 = document.createElement('div30');
+            t31 = document.createTextNode('text node 3.1');
+            e00.appendChild(e10);
+            e00.appendChild(e11);
+            e10.appendChild(t20);
+            e10.appendChild(e21);
+            e10.appendChild(t22);
+            e10.appendChild(e23);
+            e10.appendChild(t24);
+            e21.appendChild(e30);
+            e21.appendChild(t31);
+            e11.appendChild(t25);
+            e00.setAttribute('style', stl1);
+            e30.setAttribute('style', stl1);
+            e11.setAttribute('style', stl2);
+            e21.setAttribute('style', stl2);
+        });
+
+        it('find value of style property if the element has it imposed', function(){
+            // expect(dom.);
+        });
+
+
+    });
--- js/classes/HtmlElements/Dom.js
+++ js/classes/HtmlElements/Dom.js
@@ -1,6 +1,6 @@
 /*jslint white: false */
 /*jslint plusplus: true, white: true */
-/*global Node, Link, FACTORY */
+/*global Node, Dom, Styles, Tag */
 
 /**
  * This class is sort of helper to deal with DOM elements.
@@ -12,7 +12,267 @@
  */
 
 function Dom(){
+	/**
+	 * Returns value of style property `prop` of node `n`. It looks up the chain of parents until finds
+	 * first occurence of key `prop`. If the key is not present in any of ancestors, nothing is returned.
+	 * @method          _lookUpInParents
+	 * @param          {DOM.Node}           n         [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @param          {String}             prop      name of the property to look for
+	 * @return         {String|void}
+	 * @private
+	 * @since          0.0.4
+	 */
+	var _lookUpInParents = function(n, prop){
+	    console.log('inside _lookUpInParents');
+	    var currentNode = n,
+	        stl;
+	    while (currentNode){
+	        console.log('current Node: ', currentNode);
+	        if (typeof currentNode.getAttribute === 'function'){
+	            stl = new Styles(currentNode.getAttribute('style'));
+	            if (stl.hasProperty(prop)){
+	                return stl.getProperty(prop);
+	            }
+	        }
+	        currentNode = currentNode.parentNode;
+	    }
+	};
 
+	/**
+	 * Toggles inline style property `prop` of node `n`. When it turns out that the property should
+	 * be set, it is set to value `val`.
+	 *
+	 * @method          _toggleNodeStyle
+	 * @private
+	 * @param          {DOM.Node}           n          instance of [DOM.Node](https://developer.mozilla.org/en-US/docs/Web/API/Node)
+	 * @param          {String}             prop       style property name to be toggled
+	 * @param          {String}             val        on-value of the the style property
+	 * @param          {String|Null}        altVal     off-value of the style property
+	 * @return         {void}
+	 * @since          0.0.4
+	 */
+	var _toggleNodeStyle = function(n, prop, val, altVal){
+	    var attrName = 'style',
+	        stl, stlStr;
+	    if (!(typeof n === 'object' && n.nodeType)){
+	        return;
+	    }
+	    console.log('from parents: ', _lookUpInParents(n, prop));
+	    stl = new Styles(n.getAttribute(attrName));
+	    stl.toggleProperty(prop, val, altVal);
+	    stlStr = stl.toBareString();
+	    if (stlStr){
+	        n.setAttribute(attrName, stlStr);
+	    } else {
+	        n.removeAttribute(attrName);
+	    }
+	};
+
+
+	/**
+	 * Returns **proxy** node of `n`:
+	 * <ol><li>
+	 * if `n` is a text element without siblings, then proxy of `n` is its parent node,
+	 * </li><li>
+	 * if `n` is a text element with siblings or is a node element, then proxy of `n` is `n` itself.
+	 * </li></ol>
+	 * @method         _proxy
+	 * @param          {DOM.Node}          n          [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @return         {DOM.Node|Null}
+	 */
+	this.proxy = function(n){
+	    if (n === undefined || n.nodeType === undefined){
+	        return null;
+	    }
+	    if (n.nodeType === Node.ELEMENT_NODE){
+	        return n;
+	    }
+	    return (n.nextSibling || n.previousSibling) ? n : n.parentNode;
+	};
+
+	/**
+	 * Gets value of style property `key` of nearest ascendant of `startNode` up to `limitNode`
+	 * that has that property set.
+	 * If the property is not found, null is returned.
+	 * @method         getInheritedStyleProp
+	 * @param          {DOM.Node}           startNode      [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance to start
+	 * @param          {DOM.Node}           limitNode      [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @param          {String}             key            name of property to find among inline style of ascendants
+	 * @return         {String|Number|Null}
+	 */
+	// this.getInheritedStyleProp = function(key, startNode, limitNode){
+	    /// !!! stub
+	    // return null;
+	// }
+
+
+	/**
+	 * Modifies inline style properties of a {{#crossLink "Selection/_proxy:method"}}proxy{{/crossLink}}
+	 * node of deepest children of node `n` in the following way:
+	 * <ol><li>
+	 * if style property `prop` of the deepest child is equal to `val`, then inline
+	 * style property of its "proxy" node is set to be `altVal`.
+	 * </li><li>
+	 * if style property `prop` of the deepest child is not equal to `val`, then inline
+	 * style property of its "proxy" node is set to be `val`.
+	 * </li></ol>
+	 * @private
+	 * @method         _deferToggleStyle
+	 * @param          {Node}               n                  [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @param          {String}             prop               name of style property (i.e., "width" or "text-decoration")
+	 * @param          {String}             val                value of style property (i.e., "10px" or "underline")
+	 * @return         {void}
+	 * @since          0.0.4
+	 * @todo           Try to make this method shorter
+	 */
+	var _deferToggleStyle = function(n, prop, val, altVal){
+	    var childNodes = n.childNodes,
+	        childNum = childNodes.length,
+	        child, span, parent, counter;
+	    switch (childNum){
+	         // node has no children
+	        case 0:
+	            if (n.nodeType === Node.ELEMENT_NODE){
+	                _toggleNodeStyle(n, prop, val, altVal);
+	                break;
+	            }
+	            if (n.nodeType === Node.TEXT_NODE){
+	                console.log('from parents: ', _lookUpInParents(n, prop));
+	                span = new Tag();
+	                span.setTag('span');
+	                span.setStyleProperty(prop, val);
+	                span.setContent(n.nodeValue);
+	                parent = n.parentNode;
+	                if (parent){
+	                    parent.replaceChild(span.toNode(), n);
+	                }
+	                break;
+	            }
+	            break;
+	        // node has only one child
+	        case 1:
+	            child = n.firstChild;
+	            if (child.nodeType === Node.TEXT_NODE){
+	                 _toggleNodeStyle(n, prop, val, altVal);
+	            } else {
+	                _deferToggleStyle(child, prop, val, altVal);
+	            }
+	            break;
+	        // node has many children
+	        default:
+	            console.log("children no. : ", childNum, childNodes);
+	            for (counter = 0; counter < childNum; counter++){
+	                _deferToggleStyle(childNodes[counter], prop, val, altVal);
+	            }
+	    }
+	};
+
+
+	/**
+	 * Propagate style property named `prop` with the value `val` to the last descendant of each node in the selection.
+	 * Remember that the selection is in general a two-dimensional array (or one-dimensional if the selection is empty).
+	 * @method         switchDeepestChildStyle
+	 * @param          String               prop        name of the property to be imposed
+	 * @param          String               val         on-value of the above property
+	 * @param          String               altVal      off-value of the property
+	 * @since          0.0.4
+	 * @return         void
+	 */
+	this.switchDeepestChildStyle = function(prop, val, altVal){
+	    console.log("text content of selection at start: " + this.toText('|', ' ***'));
+	    var that = this;
+	    this.nodes.forEach(function(line){
+	        if (line){
+	            line.forEach(function(node){
+	                _deferToggleStyle(node.$, prop, val, altVal);
+	            });
+	        // seems to be useless
+	        that.normalizeParentOf(line);
+	        }
+	    });
+	    console.log("text content of selection at the end: " + this.toText('|', ' ***'));
+
+	};
+
+	/**
+	 * Gives common ancestor of nodes n1 and n2. If it does not exist, `null` is returned.
+	 * @method         _commonAncestor
+	 * @private
+	 * @param          {DOM.Node}           n1     [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @param          {DOM.Node}           n2     [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @return         {DOM.Node|Null}
+	 * @since          0.0.4
+	 */
+	var _commonAncestor = function(n1, n2){
+	    console.log(n1, n2);
+	    if (n1 === undefined || n2 === undefined){
+	        console.log('return undefined');
+	        return;
+	    }
+	    if (n1.contains(n2)){
+	        console.log('return first argument', n1);
+	        return n1;
+	    }
+	    if (n2.contains(n1)){
+	        console.log('return second argument', n2);
+	        return n2;
+	    }
+	    var parent = n1.parentNode;
+	    while (parent && !(parent.contains(n2))){
+	        console.log('inside while loop: ', parent);
+	        parent = parent.parentNode;
+	    }
+	    console.log('return parent', parent);
+	    return parent;
+	};
+
+	/**
+	 * Gives common ancestor of nodes n1 and n2. If it does not exist, `null` is returned.
+	 * @method         commonAncestor
+	 * @param          {DOM.Node}           n1     [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @param          {DOM.Node}           n2     [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance
+	 * @return         {DOM.Node|Null}
+	 */
+	this.commonAncestor = function(n1, n2){
+	    return _commonAncestor(n1, n2);
+	};
+
+	/**
+	 * Returns common ancestor of all array elements. If an element is not a
+	 * [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node) instance, it is ignored.
+	 * @method         commonAncestor
+	 * @return         {DOM.Node|Null}      common ancestor of the arguments
+	 */
+	this.commonAncestorSoft = function(elems){
+	    if (!Array.isArray(elems)){
+	        return null;
+	    }
+	    var arg = elems.filter(function(el){
+	        return el && el.nodeType !== undefined;
+	    });
+	    if (arg.length === 0){
+	        return null;
+	    }
+	    var el = arg.shift(),
+	        elTmp;
+	    while (arg.length > 0){
+	        elTmp = arg.shift();
+	        el = _commonAncestor(el, elTmp);
+	    }
+	    return el;
+	};
+
+	/**
+	 * Normalize parent of elements of the array.
+	 * @param  {Array}     elems
+	 * @return {void}
+	 */
+	this.normalizeParentOf = function(elems){
+	    var el = this.commonAncestorSoft(elems);
+	    if (el){
+	        el.normalize();
+	    }
+	};
 
 }
 
