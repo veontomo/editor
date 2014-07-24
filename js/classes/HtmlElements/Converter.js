8c738ba56c589ba4002452b309290c519be27fae
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-07-24 09:27:46 +0200

    Converter class is started

---
+++ js/classes/HtmlElements/Converter.js
@@ -0,0 +1,20 @@
+/*jslint plusplus: true, white: true */
+/*global Node, Dom, Properties, Tag, Helper, CKEDITOR, FACTORY, Unit, NEWSLETTER */
+
+/**
+ * Methods of this class convert one format in another.
+ * @module 	    HtmlElements
+ * @class  		Converter
+ * @since       0.0.5
+ * @author      A.Shcherbakov
+ */
+
+function Converter(){
+	"use strict";
+	if (!(this instanceof Converter)) {
+		return new Converter();
+	}
+
+
+}
+
