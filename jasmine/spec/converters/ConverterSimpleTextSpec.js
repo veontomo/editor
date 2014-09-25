1ce08bb161042c4f436213fe47699d570541c2da
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-09-25 10:43:01 +0200

    tests for simple text converter are added and pended

--- jasmine/spec/converters/ConverterSimpleTextSpec.js
+++ jasmine/spec/converters/ConverterSimpleTextSpec.js
@@ -7,7 +7,7 @@ describe ('Converter to plain text format', function(){
     	c = new ConverterSimpleText();
     });
 
-    describe('Elaborates single node document', function () {
+    describe('Elaborating single node document', function () {
         describe('Elaborates text nodes', function(){
             it('does not modify text node content', function(){
                 var t = document.createTextNode('this is a simple text node');
@@ -45,12 +45,41 @@ describe ('Converter to plain text format', function(){
                 expect(link2.nodeValue).toBe('this is a link');
             });
         });
+    });
+
+    describe('Elaborating multi node documents', function(){
+        pending();
+        it('maintains nested tags', function(){
+            var parent = document.createElement('div'),
+                child1 = document.createElement('span'),
+                child2 = document.createTextNode('text node content');
+            parent.style.color = '#012345';
+            child1.style['font-size'] = '10px';
+            child1.innerHTML = 'span content';
+            parent.appendChild(child1);
+            parent.appendChild(child2);
+            var n = c.convert(parent);
+            expect(n.nodeType).toBe(Node.ELEMENT_NODE);
+            console.log(n.outerHTML);
+        });
+
+        it('converts image tag inside div', function(){
+            var parent = document.createElement('div'),
+                img  = document.createElement('img');
+            img.setAttribute('alt', 'alternative text');
+            img.setAttribute('title', 'popup text');
+            parent.appendChild(img);
+
+            var node = c.convert(parent);
+            expect(node.nodeType).toBe(Node.ELEMENT_NODE);
+            expect(node.tagName.toLowerCase()).toBe('div');
+            expect(node.childNodes.length).toBe(1);
+            expect(node.childNodes[0].nodeType).toBe(Node.TEXT_NODE);
+            expect(node.childNodes[0].nodeValue).toBe('alternative text');
 
 
 
-
-
-
+        });
     });
 
 
