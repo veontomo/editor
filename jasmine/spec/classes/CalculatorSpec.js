11fab37d42078e41c800184dfebcfcb77f48d283
Mario Rossi <veontomo@gmail.com>
2015-04-24 09:34:37 +0200

    feat: create new version of Calculator

--- jasmine/spec/classes/CalculatorSpec.js
+++ jasmine/spec/classes/CalculatorSpec.js
@@ -1,40 +1,53 @@
 /*jslint plusplus: true, white: true */
-/*global describe, it, expect, Calculator */
+/*global describe, it, expect, beforeEach, Calculator */
 
 describe('Calculator', function(){
     var c;
     beforeEach(function(){
         c = new Calculator();
     });
-    describe('has a method calculate that', function () {
-        it('returns number 5 if its input is number 5', function () {
-            expect(c.calculate(5)).toBe(5);
+    describe('has a method "parse" that', function(){
+        it('returns object {value: 43, unit: "px"} if the input is a string "43px"', function(){
+            var obj = c.parse('43px');
+            expect(obj.value).toBe(43);
+            expect(obj.unit).toBe('px');
         });
-        it('returns number -2.98 if its input is number -2.98', function () {
-            expect(c.calculate(-2.98)).toBe(-2.98);
+        it('returns object {value: 0.121, unit: "em"} if the input is a string "0.121em"', function(){
+            var obj = c.parse('0.121em');
+            expect(obj.value).toBe(0.121);
+            expect(obj.unit).toBe('em');
         });
-        it('returns number 0 if its input is number 0', function () {
-            expect(c.calculate(0)).toBe(0);
-        });
-        it('returns string "12" if its input is string "12"', function () {
-            expect(c.calculate("12")).toBe("12");
-        });
-        it('returns string "6" if its input is string "2 + 4"', function () {
-            expect(c.calculate("2 + 4")).toBe("6");
-        });
-        it('returns string "7.32" if its input is string "8 - 0.68"', function () {
-            expect(c.calculate("8 - 0.68")).toBe("7.32");
-        });
-        it('returns string "52" if its input is string "4*13"', function () {
-            expect(c.calculate("13*4")).toBe("52");
-        });
-        it('returns string "22" if its input is string "110/5"', function () {
-            expect(c.calculate("110/5")).toBe("22");
-        });
-        it('returns string "22" if its input is string "110:5"', function () {
-            expect(c.calculate("110:5")).toBe("22");
+        it('returns object {value: -21.3, unit: "em"} if the input is a string "-21.3em"', function(){
+            var obj = c.parse('-21.3em');
+            expect(obj.value).toBe(-21.3);
+            expect(obj.unit).toBe('em');
         });
 
+        it('returns object {value: 4.6, unit: null} if the input is a string "4.6"', function(){
+            var obj = c.parse('4.6');
+            expect(obj.value).toBe(4.6);
+            expect(obj.unit).toBe(null);
+        });
+        it('returns object {value: 32, unit: null} if the input is a number 32', function(){
+            var obj = c.parse(32);
+            expect(obj.value).toBe(32);
+            expect(obj.unit).toBe(null);
+        });
+        it('returns object {value: 4.6, unit: null} if the input is a number 4.6', function(){
+            var obj = c.parse(4.6);
+            expect(obj.value).toBe(4.6);
+            expect(obj.unit).toBe(null);
+        });
+        it('returns object {value: 4.6, unit: null} if the input is a number 4.6', function(){
+            var obj = c.parse(4.6);
+            expect(obj.value).toBe(4.6);
+            expect(obj.unit).toBe(null);
+        });
+        it('returns object {value: -75.2, unit: null} if the input is a number -75.2', function(){
+            var obj = c.parse(-75.2);
+            expect(obj.value).toBe(-75.2);
+            expect(obj.unit).toBe(null);
+        });
 
 
 
@@ -42,6 +55,35 @@ describe('Calculator', function(){
 
 
     });
+    // describe('has a method calculate that', function () {
+    //     it('returns number 5 if its input is number 5', function () {
+    //         expect(c.calculate(5)).toBe(5);
+    //     });
+    //     it('returns number -2.98 if its input is number -2.98', function () {
+    //         expect(c.calculate(-2.98)).toBe(-2.98);
+    //     });
+    //     it('returns number 0 if its input is number 0', function () {
+    //         expect(c.calculate(0)).toBe(0);
+    //     });
+    //     it('returns string "12" if its input is string "12"', function () {
+    //         expect(c.calculate("12")).toBe("12");
+    //     });
+    //     it('returns string "6" if its input is string "2 + 4"', function () {
+    //         expect(c.calculate("2 + 4")).toBe("6");
+    //     });
+    //     it('returns string "7.32" if its input is string "8 - 0.68"', function () {
+    //         expect(c.calculate("8 - 0.68")).toBe("7.32");
+    //     });
+    //     it('returns string "52" if its input is string "4*13"', function () {
+    //         expect(c.calculate("13*4")).toBe("52");
+    //     });
+    //     it('returns string "22" if its input is string "110/5"', function () {
+    //         expect(c.calculate("110/5")).toBe("22");
+    //     });
+    //     it('returns string "22" if its input is string "110:5"', function () {
+    //         expect(c.calculate("110:5")).toBe("22");
+    //     });
+    // });
 
 
 });
--- js/classes/Calculator.js
+++ js/classes/Calculator.js
@@ -8,114 +8,72 @@
  * @since   0.2.7
  * @author  a.shcherbakov@ritoll.it
  */
-function Calculator(){
-	"use strict";
-	if (!(this instanceof Calculator)) {
-		return new Calculator();
-	}
+function Calculator() {
 	/**
-	 * Supported operations
-	 * @property       {Array}         _operations     array of strings representing suppored operations
+	 * Storage for the absolute value of the result of current calculation.
+	 * @type           {Number}
 	 * @since          0.2.7
 	 * @private
 	 */
-	var _operations = ['+', '-'];
+	var _memoryValue;
+
 
 	/**
-	 * Calculates expression `expr`.
-	 * @method         calculate
-	 * @param          {String|Number} expr
-	 * @return         {String|Number}
-	 * @since         0.2.7
+	 * Storage for the unit of measurement of the result of current calculation.
+	 * @type           {String|Null}
+	 * @since          0.2.7
+	 * @private
 	 */
-	this.calculate = function(expr){
-		if (typeof expr === 'number'){
-			return expr;
-		}
-		if (typeof expr === 'string'){
-			return _calculateAsString(expr);
+	var _memoryUnit;
+
+
+	/**
+	 * Initializes {{#crossLink "Calculator/_memoryValue:property"}}_memoryValue{{/crossLink}}
+	 * and {{#crossLink "Calculator/_memoryUnit:property"}}_memoryUnit{{/crossLink}} of the calculator:
+	 * if the argument is given, then the values of the above fields are set based on it,
+	 * otherwise they are set to `0` and `null` correspondingly.
+	 * @method         init
+	 * @param          {String|Number}  x  [Optional]
+	 * @return         {void}
+	 */
+	this.init = function(x){
+		var  xParsed = this.parse(x);
+		if (xParsed){
+			_memoryValue = xParsed.value;
+			_memoryUnit = xParsed.unit;
+		} else {
+			_memoryValue = 0;
+			_memoryUnit = null;
 		}
 	};
 
 	/**
-	 * Calculates expression given as a string
-	 * @method         _calculateAsString
-	 * @param          {String}        expr
-	 * @return         {String}
+	 * Returns an object with keys "value" and "unit" that represents the input argument.
+	 * @method         parse
+	 * @param          {String|Number}    x
+	 * @return         {Object|Null}
 	 * @since          0.2.7
 	 */
-	var _calculateAsString = function(expr){
-		var splitted = _cutString(expr, _operations);
-		var accum = parseInt(splitted.shift(), 10);
-		return _perform(splitted, accum).toString();
-	};
-
-	var _perform = function(arr, accum){
-		if (arr.length === 0){
-			return accum;
+	this.parse = function(x){
+		var result = {'value': null, 'unit': null};
+		if (typeof x === 'number'){
+			result.value = x;
+			return result;
 		}
-		var oper = arr.shift();
-		var accum2 = parseInt(arr.shift(), 10);
-		switch (oper){
-			case '+':
-				return accum + _perform(arr, accum2);
-			case '-':
-				return accum - _perform(arr, accum2);
-
+		var xTrimmed = x.trim();
+		var value = parseFloat(xTrimmed, 10);
+		if (isNaN(value)){
+			return;
 		}
-	}
-
-	/**
-	 * Cuts string into pieces. Places of cuts are given by array `marks`.
-	 * @method         _split
-	 * @param          {String}        str
-	 * @param          {Array}         marks
-	 * @return         {Array}
-	 * @since          0.2.7
-	 */
-	var _cutString = function(str, marks){
-		var output = [],
-			pos = _positions(str, marks),
-			len = pos.length,
-			i,
-			pointer = 0;
-		console.log(pos);
-		if (len === 0){
-			return [str];
+		result.value = value;
+		var unit = xTrimmed.replace(value.toString(), '').trim();
+		if (unit.length > 0){
+			result.unit = unit;
 		}
-		for (i = 0; i < len; i++){
-			console.log(i, pos[i], pointer);
-			output.push(str.slice(pointer, pos[i]).trim());
-			output.push(str.charAt(pos[i]));
-			pointer = pos[i] + 1;
-		}
-		output.push(str.slice(pointer).trim());
-		return output;
-	};
-
-	/**
-	 * Returns array of increasing integers that indicate positions at which
-	 * single-symbol strings in `marks` occur in `str`.
-	 * @method         _positions
-	 * @param          {String}        str
-	 * @param          {Array}         marks
-	 * @return         {Array}
-	 * @since          0.2.7
-	 */
-	var _positions = function(str, marks){
-		var i,
-			len = str.length,
-			output = [];
-		for (i = 0; i < len; i++){
-			if (marks.indexOf(str.charAt(i)) !== -1){
-				output.push(i);
-			}
-		}
-		return output;
-
+		return result;
 
 	};
-
-
 
 }
+
+
